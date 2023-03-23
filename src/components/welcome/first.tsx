065e879c6f4cfc5e31fbfc47a96bd96a638abcd8
王云阳 <511763880@qq.com>
2023-03-23 15:55:39 +0800

    feat. 创建欢迎页面

---
+++ src/components/welcome/first.tsx
@@ -0,0 +1,6 @@
+import { defineComponent } from "vue";
+export const First = defineComponent({
+    setup: (props, context) => {
+        return () => <div>First</div>;
+    },
+});
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,3 +1,21 @@
+import { RouteRecordRaw } from "vue-router";
 import { App } from "../App";
+import { First } from "../components/welcome/first";
+import { Forth } from "../components/welcome/forth";
+import { Second } from "../components/welcome/second";
+import { Third } from "../components/welcome/third";
+import { Welcome } from "../views/Welcome";
 
-export const routes = [{ path: "/", component: App }];
+export const routes: RouteRecordRaw[] = [
+    { path: "/", component: App },
+    {
+        path: "/welcome",
+        component: Welcome,
+        children: [
+            { path: "1", component: First },
+            { path: "2", component: Second },
+            { path: "3", component: Third },
+            { path: "4", component: Forth },
+        ],
+    },
+];
