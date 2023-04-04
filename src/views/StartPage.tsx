50e4320d3b0ccffd3c6a7a17e757f6d5b1d14504
王云阳 <511763880@qq.com>
2023-04-04 15:06:42 +0800

    修复iconName为空的bug

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -14,7 +14,7 @@ export const StartPage = defineComponent({
                         测试
                     </Button>
                 </div>
-                <FloatButton />
+                <FloatButton iconName="add" />
             </div>
         );
     },
